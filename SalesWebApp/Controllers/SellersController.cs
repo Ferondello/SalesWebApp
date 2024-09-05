09a7607db2cadf4d99bb9cc3736ef827e16b66ab
Felipe Rondello <ferondellodev@gmail.com>
2024-09-05 17:02:22 -0300

    Delete seller

--- SalesWebApp/Controllers/SellersController.cs
+++ SalesWebApp/Controllers/SellersController.cs
@@ -24,7 +24,7 @@ namespace SalesWebApp.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindALl();
-            var viewModel =  new SellerFormViewModel { Departments = departments };
+            var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
 
@@ -33,6 +33,29 @@ namespace SalesWebApp.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
