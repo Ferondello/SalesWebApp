cbd60379263d15e8460e41a8eb48a0cc3d3c761c
Felipe Rondello <ferondellodev@gmail.com>
2024-09-05 10:45:25 -0300

    Foreign key not null

---
+++ SalesWebApp/Migrations/20240905134021_DepartementForeignKey.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartementForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
