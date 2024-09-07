e3a35a3362ffd3e5c3fc09551bed0a322e102c13
Felipe Rondello <ferondellodev@gmail.com>
2024-09-07 15:34:28 -0300

    Update sellers and custom service exception

--- SalesWebApp/Services/SellerService.cs
+++ SalesWebApp/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using SalesWebApp.Data;
 using SalesWebApp.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesWebApp.Services.Exceptions;
 
 namespace SalesWebApp.Services
 {
@@ -33,6 +34,23 @@ namespace SalesWebApp.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
